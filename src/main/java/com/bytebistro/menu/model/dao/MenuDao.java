16dd77ef91bc97a24f6a60cfe8e4b152c20ff315
Badal Shrestha <shresthabadal324@gmail.com>
2026-05-02 14:40:42 +0545

    made the remaining methods to static

--- src/main/java/com/bytebistro/menu/model/dao/MenuDao.java
+++ src/main/java/com/bytebistro/menu/model/dao/MenuDao.java
@@ -2,9 +2,7 @@ package com.bytebistro.menu.model.dao;
 
 import com.bytebistro.menu.model.MenuItem;
 import com.bytebistro.utils.DBConnection;
-import com.mysql.cj.xdevapi.DbDoc;
 
-import java.awt.*;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -14,15 +12,15 @@ import java.util.List;
 
 public class MenuDao {
 
-    public static boolean insertItem(MenuItem menuObj) throws SQLException{
+    public static boolean insertMenuItem(MenuItem item) throws SQLException{
         String query = "INSERT INTO menu_item (name, description, price, item_type, is_available) VALUES (?, ?, ?, ?, ?)";
         try (Connection conn = DBConnection.getConnection();
              PreparedStatement st = conn.prepareStatement(query)) {
-            st.setString(1, menuObj.getName());
-            st.setString(2, menuObj.getDescription());
-            st.setDouble(3, menuObj.getPrice());
-            st.setString(4, menuObj.getItemType());
-            st.setBoolean(5, menuObj.isAvailable());
+            st.setString(1, item.getName());
+            st.setString(2, item.getDescription());
+            st.setDouble(3, item.getPrice());
+            st.setString(4, item.getItemType());
+            st.setBoolean(5, item.isAvailable());
 
             int insertedRows = st.executeUpdate();
             if (insertedRows == 0) {
@@ -33,27 +31,27 @@ public class MenuDao {
         }
     }
 
-    public static List<MenuItem> fetchMenu() throws SQLException{
+    public static List<MenuItem> fetchMenuItems() throws SQLException{
         String query = "SELECT * FROM menu_items";
         try (Connection conn = DBConnection.getConnection();
              PreparedStatement st = conn.prepareStatement(query)) {
 
             ResultSet rs = st.executeQuery();
 
-            List<MenuItem> items = new ArrayList<>();
+            List<MenuItem> menuList = new ArrayList<>();
 
             while (rs.next()) {
-                int id = rs.getInt(1);
+                int itemId = rs.getInt(1);
                 String name = rs.getString(2);
                 String description = rs.getString(3);
                 double price = rs.getDouble(4);
                 String itemType = rs.getString(5);
                 boolean isAvailable = rs.getBoolean(6);
 
-                MenuItem m = new MenuItem(id, name, description, price, itemType, isAvailable);
-                items.add(m);
+                MenuItem item = new MenuItem(itemId, name, description, price, itemType, isAvailable);
+                menuList.add(item);
             }
-            return items;
+            return menuList;
         }
     }
 
@@ -80,7 +78,7 @@ public class MenuDao {
         }
     }
 
-    public static boolean updateItem(MenuItem item) throws SQLException{
+    public static boolean updateMenuItem(MenuItem item) throws SQLException{
         String query = "UPDATE menu_items SET name = ?, description = ?, price = ?, item_type = ?, is_available = ? WHERE item_id = ?";
 
         try (Connection conn = DBConnection.getConnection();
@@ -102,7 +100,7 @@ public class MenuDao {
         }
     }
 
-    public boolean deleteItem(int id) throws SQLException{
+    public static boolean deleteMenuItem(int id) throws SQLException{
         String query = "DELETE FROM menu_items WHERE item_id = ?";
 
         try (Connection conn = DBConnection.getConnection();
@@ -119,8 +117,8 @@ public class MenuDao {
         }
     }
 
-    public List<MenuItem> searchItems(String keyword) throws SQLException{
-        //List<MenuItem> items = new
+    public static List<MenuItem> searchMenuItems(String keyword) throws SQLException{
+
         String query = "SELECT * FROM menu_items WHERE name LIKE ? OR item_type LIKE ?";
         try (Connection conn = DBConnection.getConnection();
                 PreparedStatement st = conn.prepareStatement(query)) {
@@ -129,21 +127,21 @@ public class MenuDao {
 
             ResultSet rs = st.executeQuery();
 
-            List<MenuItem> items = new ArrayList<>();
+            List<MenuItem> menuList = new ArrayList<>();
 
             while (rs.next()) {
-                int id = rs.getInt("item_id");
+                int itemId = rs.getInt("item_id");
                 String name = rs.getString("name");
                 String description = rs.getString("description");
                 double price = rs.getDouble("price");
                 String itemType = rs.getString("item_type");
                 boolean isAvailable = rs.getBoolean("is_available");
 
-                MenuItem m = new MenuItem(id, name, description, price, itemType, isAvailable);
-                items.add(m);
+                MenuItem item = new MenuItem(itemId, name, description, price, itemType, isAvailable);
+                menuList.add(item);
 
             }
-            return items;
+            return menuList;
         }
     }
 }
