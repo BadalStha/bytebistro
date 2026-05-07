ac6ed6251f308e7cd3bfd883e323595ef00d7167
Badal Shrestha <shresthabadal324@gmail.com>
2026-05-07 08:16:16 +0545

    created order attributes, getter and setter

--- src/main/java/com/bytebistro/order/model/Order.java
+++ src/main/java/com/bytebistro/order/model/Order.java
@@ -1,4 +1,59 @@
 package com.bytebistro.order.model;
 
 public class Order {
+    private int orderId;
+    private int userId;
+    private String deliveryAddress;
+    private String status;
+    private String orderedAt;
+
+    public Order() {}
+
+    public Order(int orderId, int userId, String deliveryAddress, String status, String orderedAt) {
+        this.orderId = orderId;
+        this.userId = userId;
+        this.deliveryAddress = deliveryAddress;
+        this.status = status;
+        this.orderedAt = orderedAt;
+    }
+
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(int orderId) {
+        this.orderId = orderId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getDeliveryAddress() {
+        return deliveryAddress;
+    }
+
+    public void setDeliveryAddress(String deliveryAddress) {
+        this.deliveryAddress = deliveryAddress;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getOrderedAt() {
+        return orderedAt;
+    }
+
+    public void setOrderedAt(String orderedAt) {
+        this.orderedAt = orderedAt;
+    }
 }
