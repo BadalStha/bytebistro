9b949ba8556bac8f07f4cdf62496c302fd3978a9
Badal Shrestha <shresthabadal324@gmail.com>
2026-05-07 08:18:22 +0545

    created orderitem with necessary attributes and getter setter

--- src/main/java/com/bytebistro/order/model/OrderItem.java
+++ src/main/java/com/bytebistro/order/model/OrderItem.java
@@ -1,4 +1,59 @@
 package com.bytebistro.order.model;
 
 public class OrderItem {
+    private int orderItemId;
+    private int orderId;
+    private int itemId;
+    private int quantity;
+    private double unitPrice;
+
+    public OrderItem() {}
+
+    public OrderItem(int orderItemId, int orderId, int itemId, int quantity, double unitPrice) {
+        this.orderItemId = orderItemId;
+        this.orderId = orderId;
+        this.itemId = itemId;
+        this.quantity = quantity;
+        this.unitPrice = unitPrice;
+    }
+
+    public int getOrderItemId() {
+        return orderItemId;
+    }
+
+    public void setOrderItemId(int orderItemId) {
+        this.orderItemId = orderItemId;
+    }
+
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(int orderId) {
+        this.orderId = orderId;
+    }
+
+    public int getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(int itemId) {
+        this.itemId = itemId;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getUnitPrice() {
+        return unitPrice;
+    }
+
+    public void setUnitPrice(double unitPrice) {
+        this.unitPrice = unitPrice;
+    }
 }
